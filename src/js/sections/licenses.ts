af81f4dc0eb73321233556ef76066319925d54ed
Artem Safin <artemsafin.webdev@gmail.com>
2026-03-02 20:59:55 +0300

    По слайдеру сертификатов

--- src/js/sections/licenses.ts
+++ src/js/sections/licenses.ts
@@ -1,4 +1,5 @@
 import Swiper from "swiper";
+import { Navigation } from "swiper/modules";
 
 export default function licenses() {
   const elements = Array.from(
@@ -7,9 +8,19 @@ export default function licenses() {
   elements.forEach((element) => {
     const container = element.querySelector<HTMLElement>(".swiper");
     if (!container) return;
+
     new Swiper(container, {
       speed: 600,
       slidesPerView: "auto",
+      modules: [Navigation],
+      navigation: {
+        prevEl: element.querySelector<HTMLButtonElement>(
+          ".slider-nav__arrow--prev",
+        ),
+        nextEl: element.querySelector<HTMLButtonElement>(
+          ".slider-nav__arrow--next",
+        ),
+      },
     });
   });
 }
