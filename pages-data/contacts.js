98e9375e4ab7b1d9efba9bdb40221b3c1c3a9952
Artem Safin <artemsafin.webdev@gmail.com>
2026-03-10 02:35:08 +0300

    Заложены основы контактов

---
+++ pages-data/contacts.js
@@ -0,0 +1,45 @@
+const contactsPageData = {
+  "/contacts.html": {
+    title: "Контакты",
+    contactsSocials: {
+      title: "Социальные сети",
+      lead: "Мы максимально открыты и подтверждаем экспертность в соц.сетях",
+      items: [
+        {
+          name: "YouTube",
+          href: "#",
+          followers: "2 500 подписчиков",
+          iconType: "youtube",
+          iconSrc: "/images/contacts-socials/youtube.svg",
+          iconAlt: "Иконка YouTube",
+        },
+        {
+          name: "ВКонтакте",
+          href: "#",
+          followers: "2 500 подписчиков",
+          iconType: "vk",
+          iconSrc: "/images/contacts-socials/vk.svg",
+          iconAlt: "Иконка ВКонтакте",
+        },
+        {
+          name: "RuTube",
+          href: "#",
+          followers: "2 500 подписчиков",
+          iconType: "rutube",
+          iconSrc: "/images/contacts-socials/rutube.svg",
+          iconAlt: "Иконка RuTube",
+        },
+        {
+          name: "Telegram",
+          href: "#",
+          followers: "2 500 подписчиков",
+          iconType: "telegram",
+          iconSrc: "/images/contacts-socials/telegram.svg",
+          iconAlt: "Иконка Telegram",
+        },
+      ],
+    },
+  },
+};
+
+export default contactsPageData;
