db401bcb56a6089284f9ea49883001385f658572
Artem Safin <artemsafin.webdev@gmail.com>
2026-03-12 04:08:07 +0300

    По ховерам

--- src/js/sections/yandexMaps.ts
+++ src/js/sections/yandexMaps.ts
@@ -124,7 +124,6 @@ export default async function initYandexMap(
         size: "normal",
         iconName: markerIcon,
         color: markerColor,
-        title: point.title,
       }),
     );
   });
