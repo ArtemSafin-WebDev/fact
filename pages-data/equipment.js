a34ecbadf4da0a13a30657b4adc7f84828f9d86f
Artem Safin <artemsafin.webdev@gmail.com>
2026-04-19 00:36:15 +0300

    По страницам

---
+++ pages-data/equipment.js
@@ -0,0 +1,15 @@
+const equipmentPageData = {
+  "/equipment.html": {
+    title: "Оборудование",
+    equipmentPage: {
+      breadcrumbs: [
+        { title: "Главная", href: "/" },
+        { title: "О клинике", href: "/about.html" },
+        { title: "Оборудование", isCurrent: true },
+      ],
+      title: "Оборудование",
+    },
+  },
+};
+
+export default equipmentPageData;
